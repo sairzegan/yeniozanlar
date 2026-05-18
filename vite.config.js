05b59a73e7f2bbd376ec5af7bc5f5418562edb9a
sairzegan <sairzegan@gmail.com>
2026-05-19 02:43:48 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic'
+  })],
   build: {
-    target: 'es2015',
-    minify: 'esbuild',
+    target: 'esnext',
+    minify: false
   }
 })
